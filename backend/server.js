f1ba205827bb7068bd4647053a210bde139d44d0
elias29 <eliasfilip.ef@gmail.com>
2020-11-28 00:32:30 +0100

    once again

--- backend/server.js
+++ backend/server.js
@@ -20,17 +20,8 @@ app.post('/test', function (req, res) {
    res.send('Hello POST');
 })
 
-const sql = postgres('postgres://username:password@host:port/database', {
-  host        : 'db-postgresql-fra1-54507-do-user-8314748-0.b.db.ondigitalocean.com',         // Postgres ip address or domain name
-  port        : 25060,       // Postgres server port
-  database    : 'internatnyportal',         // Name of database to connect to
-  username    : 'doadmin',         // Username of database user
-  password    : 'ms9axdl0dx93krdb',         // Password of database user
-  ssl         : false      // True, or options for tls.connect
-})
+const sql = postgres('postgres://doadmin:ms9axdl0dx93krdb@db-postgresql-fra1-54507-do-user-8314748-0.b.db.ondigitalocean.com:25060/internatnyportal')
 
-
- 
  app.get('/typy', async function (req, res) {
 	var tmp =  await sql`select * from ciselnik_typ`
    res.send(tmp);
