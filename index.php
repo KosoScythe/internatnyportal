248f02fb8b4a0169884aded1c69267685f991e9a
Maros Maly <marosmaly97@gmail.com>
2020-12-06 22:46:04 +0100

    Mobilna verzia
    upravena mobilna verzia podla pripomienok

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 
 <html lang="sk">
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
 		<title> Internátny portál </title>
 		<link rel="shortcut icon" type="png" href= "img/icon.png" >
 		
-		<link rel="stylesheet" media="screen and (max-width: 899px)" href="mobil.css">	
 		<link rel="stylesheet" media="screen and (min-width: 900px)" href="css.css">
+		<link rel="stylesheet" media="screen and (max-width: 899px)" href="mobil.css">			
 		
 		<link rel="stylesheet" media="screen" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
